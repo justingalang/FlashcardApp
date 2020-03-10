ca4b8716463d66d4cb8817139fe122ec66c9bf85
Justin Galang <justingalang@gmail.com>
2020-03-09 21:03:11 -0500

    Initial Commit

---
+++ FlashcardWeek5/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlashcardWeek5
+//
+//  Created by Justin Galang on 3/9/20.
+//  Copyright © 2020 Justin Galang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
